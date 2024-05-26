20cba3c3fad42abe1fc12cd2fc4082a0292883ad
wallace <wallacemesqti@gmail.com>
2024-05-26 17:37:41 -0300

    Atualização do main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello Word")
+# Crie um programa que o usuário digita o seu nome e retorna o número de caractares
+
+digite_seu_nome = input("Digite seu nome")
+
+print(digite_seu_nome)
+print(len(digite_seu_nome))
+
+
+# Crie um programa onde o usuário digite dois valores e apareça a soma
+
+primeiro_numero = int(input('Digite o primeiro numero: '))
+segundo_numero = int(input('Digite o segundo numero: '))
+
+print(primeiro_numero + segundo_numero)
+
+_SENHA_AWS = 4587215 # Senha secreta
